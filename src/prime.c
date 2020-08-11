13f41133e7e2a604c79a373f6ed3eda88c6ac4eb
stepin104644 <69044774+stepin104644@users.noreply.github.com>
2020-08-11 23:34:16 +0530

    Update prime.c

--- src/prime.c
+++ src/prime.c
@@ -1,14 +1,12 @@
-/// @file prime.c
-
 #include "prime.h"
 #include<math.h>
 
-int flag=1; //< flag variable to set 0 if not prime
+int flag=1; // flag variable to set 0 if not prime
 
 int prime(int number)
 {
-        int loop_var;
-
+   int loop_var;
+        
     if(number<2)  //< neither prime nor composite
       return -1;
 
@@ -16,15 +14,13 @@ int prime(int number)
       {
          for(loop_var=2;loop_var<=sqrt(number);loop_var++)
           {
-              if((number%loop_var)==0)
+             if((number%loop_var)==0)
                 {
                     flag=0; //< not a prime number
                      break;
                 }
            }
-     }
+       }
 
     return flag;
-
-
 }
